fb8df5c8d109c17d9338b61c77b90559fbbf0673
itengineers99 <itengineers99>
2026-05-07 21:14:45 +0530

    latest commit happened

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello()
 	{
-		return "welcome to microservices world-by-srinivasarau";
+		return "welcome to microservices world-by-srinivasaraju";
 	}
 
 }

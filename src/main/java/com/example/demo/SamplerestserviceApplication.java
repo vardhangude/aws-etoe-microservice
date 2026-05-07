4e6fa50e7dce324091756eca95b55e174ec22fb9
itengineers99 <itengineers99>
2026-05-07 19:19:47 +0530

    Initial commit happen

---
+++ src/main/java/com/example/demo/SamplerestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplerestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplerestserviceApplication.class, args);
+	}
+
+}
